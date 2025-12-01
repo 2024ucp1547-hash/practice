7259dacdf6036a454879bd9dc71513c507fda8de
unknown <2020ucp1547@mnit.ac.in>
2025-12-01 12:16:29 +0530

    in kp

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double a, b, product;
+
+    cout << "Enter first number: ";
+    cin >> a;
+
+    cout << "Enter second number: ";
+    cin >> b;
+
+    product = a * b;
+
+    cout << "Product = " << product << endl;
+
+    return 0;
+}
